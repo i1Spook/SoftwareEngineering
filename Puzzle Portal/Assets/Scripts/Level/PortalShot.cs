c866af33ef3214b5e4e8e9c3599eb3fa8c268a4a
Andrew73174 <ChamberlainAn73174@th-nuernberg.de>
2018-11-14 20:41:19 +0100

    Changed the Portal Script

--- Puzzle Portal/Assets/Scripts/Level/PortalShot.cs
+++ Puzzle Portal/Assets/Scripts/Level/PortalShot.cs
@@ -2,28 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PortalShot : MonoBehaviour {
+public class PortalShot : MonoBehaviour
+{
 
     public GameObject BluePortal;
-
-    public bool Hit = false;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    public float winkel;
+    public Vector3 Showme;
+    Quaternion Rotation;
+    // Use this for initialization
+    void Start()
     {
-		
-	}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
     void OnCollisionEnter2D(Collision2D CollisionWith)
     {
-        Hit = true;
-        Quaternion Rotation =  CollisionWith.gameObject.transform.rotation;
-        //GameObject BluePortalCopy = Instantiate(BluePortal, transform.position ,Rotation);
+        Rotation = CollisionWith.gameObject.transform.rotation;
+        winkel = Rotation.eulerAngles.z;
         BluePortal.transform.position = transform.position;
-        BluePortal.transform.rotation = Rotation * Quaternion.Euler(0,0,180f);
+
+        Showme = transform.right;
+
+        var angle = 180f;
+        if (((winkel == 90) || (winkel == -90)) && (transform.right.y > 0f))
+        {
+            angle = 0f;
+        }
+        else if ((winkel == 0) && (transform.right.x > 0f))
+        {
+            angle = 0f;
+        }
+
+
+            BluePortal.transform.rotation = Rotation * Quaternion.Euler(0, 0, angle);
 
         Destroy(gameObject);
     }
