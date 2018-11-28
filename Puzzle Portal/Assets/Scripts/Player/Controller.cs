d9a8fbc5c1c3206c1cf2390a98b9a940f8333c15
mawy1 <44568196+mawy1@users.noreply.github.com>
2018-11-28 18:52:01 +0100

    Add files via upload
    Update Player Controller. Einfach dem Player hinzufügen

---
+++ Puzzle Portal/Assets/Scripts/Player/Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+	public float maxSpeed = 10f;
+	bool facingRight = true;
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+	{		
+		float move = Input.GetAxis ("Horizontal");
+
+		rb.velocity = new Vector2 (move * maxSpeed, rb.velocity.y);
+
+		if (move > 0 && !facingRight) {
+			Flip ();
+		} else if (move < 0 && facingRight) {
+			Flip ();
+		}
+	}
+
+	 void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+	    transform.localScale = theScale;
+	}
+}
